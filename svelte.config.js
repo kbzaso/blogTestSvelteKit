12270358e77bc2e623a0f7ba2bd9133ae79aa931
kbzaso <alejandro.sapa@gmail.com>
2021-12-31 17:39:26 -0300

    fix: deploy vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,12 @@
 import preprocess from 'svelte-preprocess';
-// import adapter from '@sveltejs/adapter-static';
-// import adapter from '@sveltejs/adapter-auto';
-import vercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: vercel(),
-
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		adapter: adapter()
 	},
 
 	preprocess: [preprocess({})]
